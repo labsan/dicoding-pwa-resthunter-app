7d793da1808108cc5ad4aac0a3fd5d8fa9bb6bc5
labsan <yondeveloper@gmail.com>
2021-07-20 21:18:31 +0700

    Update styles folder

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,19 @@
 /* for async await transpile */
 import 'regenerator-runtime';
 
-/* import elements CSS */
+/* import CSS */
 import '../styles/root.css';
+
 import '../styles/elements/header.css';
+import '../styles/elements/nav.css';
 import '../styles/elements/main.css';
 import '../styles/elements/footer.css';
 
-/* import responsives CSS */
-import '../styles/responsives/small-devices.css';
-import '../styles/responsives/medium-devices.css';
-import '../styles/responsives/large-devices.css';
+import '../styles/elements/mains/detail-resto-layout.css';
+import '../styles/elements/mains/fav-resto-layout.css';
+import '../styles/elements/mains/spinner.css';
+
+import '../styles/responsives/responsive.css';
 
 /* Import Custom Element */
 import './components/navbar';
@@ -18,19 +21,26 @@ import './components/hero';
 import './components/footer';
 
 /* import JS */
-import App from './views/app'; 
+import App from './views/app';
+import SW_REGISTER from './utils/sw-register';
+import WEBSOCKET_INITIATOR from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
-/* Init App */ 
+/* Init App */
 const APP = new App({
-    button: document.querySelector('.menu'),
-    drawer: document.querySelector('.nav-list'),
-    content: document.querySelector('#main-content'),
+  button: document.querySelector('.menu'),
+  drawer: document.querySelector('.nav-list'),
+  content: document.querySelector('#main-content'),
 });
 
 window.addEventListener('hashchange', () => {
-    APP.renderPage();
+  document.querySelector('.container').scrollIntoView();
+  APP.renderPage();
 });
 
 window.addEventListener('load', () => {
-    APP.renderPage();
+  APP.renderPage();
+  // eslint-disable-next-line new-cap
+  SW_REGISTER();
+  WEBSOCKET_INITIATOR.init(CONFIG.WEBSOCKET_SERVER);
 });
